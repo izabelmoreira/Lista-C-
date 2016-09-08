5bf2bbd8f32faabfbf434a9edacda5eea7e7d459
Izabel Assunção <izabelassuncaom@gmail.com>
2016-09-08 16:21:16 -0300

    Add files via upload

---
+++ Questao01.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n1, n2, result;
+            Console.WriteLine("Escreva um número");
+
+            n1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Escreva outro número");
+            n2 = Convert.ToDouble(Console.ReadLine());
+            result = n1 - n2;
+            Console.WriteLine("o resultado é  " +result);
+
+        }
+    }
+}
